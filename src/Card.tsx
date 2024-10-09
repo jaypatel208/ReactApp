062faaa91b66211376a880452a83dc2eeae3ca9a
Jay Patel <jay037493@gmail.com>
2024-10-09 21:41:53 +0530

    Card done

---
+++ src/Card.tsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card">
+            <img className="card-image" src="https://avatars.githubusercontent.com/u/83394162?v=4" alt="Profile Picture"></img>
+            <h2 className="card-title">jaypatel_208</h2>
+            <p className="card-text">I am native android developer and tech enthusiast.</p>
+        </div>
+    );
+}
+
+export default Card
